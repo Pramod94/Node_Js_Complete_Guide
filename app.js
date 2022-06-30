fb85dd67b645fc6722d7147e5a2db3bca80e284a
PramodaM4444 <Pramoda.M@ibm.com>
2022-06-30 17:59:56 +0530

    Handling request based on the api endpoint and method

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-// Here we are importing the module called 'http'
-const http = require("http");
-
 const express = require("express");
+const fs = require("fs");
+
+// This module is required to parse the incoming data
+const bodyParser = require("body-parser");
 
 const app = express();
 
-// request will be handled in a sequence by "use"
+// This will helps to parse the incoming response body
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// "use" will look for every request
-app.use((req, res, next) => {
-  console.log("From the middleware...!");
-  next(); // next will allow to execute the subsequent "use" call
+app.use("/add-product", (req, res, next) => {
+  console.log("From the path '/add-product'");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
 });
 
-// This will be executed only if we specify the next in previous "use" call
-app.use((req, res, next) => {
-  console.log("From the another middleware...!!!");
-  res.send("<h1>Hello from Express js...!!!</h1>");
+// To restrict the Usage of api end point only to GET, POST, PUT or DELETE requests
+// We can make use of app.get(), app.post(), app.put() or app.delete() instead of app.use()
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  fs.writeFileSync("Book", req.body.title);
+  res.redirect("/");
 });
 
-// http module has a method to create a server
-// createServer will accept a function with request and response
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  console.log("From the path '/'");
+  res.send('<h1>From the Next js on path "/"</h1>');
+});
 
-// To start the server we need to specify the port
-server.listen(8080);
+app.listen(8080);
