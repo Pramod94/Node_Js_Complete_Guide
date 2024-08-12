e3ade0cd37cb72345d0f9b615af3f3c7b8c8207b
Pramod M <pramodmithyantha@gmail.com>
2024-08-12 20:15:31 +0530

    Setting and accessing Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,8 @@
+// Note : Cookies are stored on the Client side and Sessions are stored on the server side
+
 exports.getLogin = (req, res, next) => {
+  // This way we can access the Cookie sent by all API's
+  console.log("Cookie", req.get("Cookie"));
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
@@ -7,5 +11,9 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
+  // This will set the cookie "isLoggedIn=true", which can be tested in the browser (Application -> Cookie)
+  // IMP - This cookie will be attached to the every request sent by the browser. This can be checked in
+  // the request header of all the incoming request i.e all API from the browser to the server
+  res.setHeader("Set-Cookie", "isLoggedIn=true");
   res.redirect("/");
 };
